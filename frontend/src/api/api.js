a3009f1ea385bac629268fce8f4a820de9bd01e4
Johnki1 <garciakider@gmail.com>
2025-05-30 02:42:06 -0500

    1

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://54.237.150.146/api',
   headers: {
     'Content-Type': 'application/json',
   },
