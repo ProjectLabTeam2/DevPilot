6a5a2d853d0703bcd1fb9b7cccd827e14b71b1b1
John Alzate <157264782+Johnki1@users.noreply.github.com>
2025-05-30 16:36:44 -0500

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://35.170.109.174:443/api',
+  baseURL: 'https://35.170.109.174:80/api',
   headers: {
     'Content-Type': 'application/json',
   },
