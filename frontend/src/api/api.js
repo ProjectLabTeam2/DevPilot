8f56c9a0494ad2059897c960090f3704cfbe9ea6
ElzJuanjo <juanjojaramillovera@gmail.com>
2025-05-30 20:15:15 -0500

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://52.202.178.61:80/api',
+  baseURL: 'https://52.202.178.61:80/api',
   headers: {
     'Content-Type': 'application/json',
   },
